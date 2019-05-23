bbc6095ad9db8a225813f4cff65a311265110517
David Van Beveren <david.vanbeveren@gmail.com>
2019-05-23 16:00:12 +0200

    Patreon Link Update

--- welcome.php
+++ welcome.php
@@ -1,5 +1,5 @@
 	<div class="blog-carousel-content" style="padding: 20px 40px;margin-bottom: 30px;">              
 	<b><i class="fas fa-pig"></i></b><b>&nbsp;&nbsp;Welcome to <a href="https://VeganActivism.org" style="color:#ff0097;">VeganActivism.org</a></b>, the most comprehensive verified list of Vegan Activism organizations online for Vegans looking to jump into activism. Whether you have a lot of time or little, whether you're new or experienced, there's an organization here that's looking for your help to spread our compassionate vegan message.
 	<br> <br>
-	<i> This site is an ad-less project by <a href="https://YouAreTheirVoice.com" target="_blank" style="color:#ff0097;">YouAreTheirVoice.com</a>, which is funded by its Patreon supporters. Please <a href="https://www.patreon.com/youaretheirvoice" target="_blank" style="color:#ff0097;">consider a donation</a> if you find our site useful!</i>&nbsp;&nbsp;<i class="fas fa-hand-holding-heart"></i>
+	<i> This site is an ad-less project by the <a href="https://VeganHacktivists.org" target="_blank" style="color:#ff0097;">VeganHacktivists.org</a>, which is funded by its Patreon supporters. Please <a href="https://www.patreon.com/veganhacktivists" target="_blank" style="color:#ff0097;">consider a donation</a> if you find our site useful!</i>&nbsp;&nbsp;<i class="fas fa-hand-holding-heart"></i>
 	</div>
