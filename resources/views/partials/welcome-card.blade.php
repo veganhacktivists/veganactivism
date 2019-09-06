1fcdc2661578289de3aa1a7bc44fe47250410ea9
MariaHCD <mariahcdcosta@gmail.com>
2019-09-06 21:06:56 +0400

    Add welcome card

--- resources/views/partials/welcome-card.blade.php
+++ resources/views/partials/welcome-card.blade.php
@@ -1,3 +1,15 @@
-<div class="card p-5 my-3">
-  Welcome to VeganActivism.org card
+<div class="card my-3">
+  <div class="card-body">
+    <b><i class="fas fa-leaf"></i></b><b>&nbsp;&nbsp;
+      Welcome to <a href="https://VeganActivism.org" class="primary-color">VeganActivism.org</a></b>,
+    the most comprehensive verified list of Vegan Activism organizations online for Vegans looking to jump into activism.
+
+    Whether you have a lot of time or little, whether you're new or experienced, there's an organization here that's looking for your help to spread our compassionate vegan message.
+    <br> <br>
+    <i> This site is an ad-less project by the
+      <a href="https://VeganHacktivists.org" target="_blank" class="primary-color">VeganHacktivists.org</a>,
+      which is funded by its Patreon supporters.
+      Please <a href="https://www.patreon.com/veganhacktivists" target="_blank" class="primary-color">consider a donation</a>
+      if you find our site useful!</i>&nbsp;&nbsp;<i class="fas fa-hand-holding-heart"></i>
+  </div>
 </div>
