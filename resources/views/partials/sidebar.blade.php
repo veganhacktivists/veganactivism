59a429d0c9af4f614c01e2abf94447b227892c58
MariaHCD <mariahcdcosta@gmail.com>
2019-09-06 21:37:38 +0400

    Add logo and activism newsletter cards

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,6 +1,42 @@
-<div class="card p-5 my-3">veganactivism.org card</div>
+<div class="card my-3">
+    <div class="card-body">
+        <a href="https://veganactivism.org"><img src="https://i.imgur.com/NgQ72ay.png" class="img-full"></a>
+    </div>
+</div>
 
-<div class="card p-5 my-3">activism newsletter card</div>
+<div class="card my-3 p-4 bg-color-gray">
+    <h5 class="card-title"><i class="fas fa-bell"></i>&nbsp;&nbsp;
+        Activism Newsletter
+    </h5>
+    <ul class="list-unstyled post-simple-list mb-0" style="padding: 0px 15px 0px 6px;">
+
+        <!-- Begin Mailchimp Signup Form -->
+        <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+        <style type="text/css">
+            #mc_embed_signup {
+                background: #fff;
+                clear: left;
+                font: 14px Helvetica, Arial, sans-serif;
+            }
+
+            /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+        </style>
+        <div id="mc_embed_signup">
+            <form style="background-color: #f9f9f9;" action="https://youaretheirvoice.us20.list-manage.com/subscribe/post?u=0baba35be8f6397f7ac1066f1&amp;id=5fd11d4221" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <div id="mc_embed_signup_scroll">
+
+                    <input style="width: 100%;" type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Enter your Email Address" required>
+                    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_0baba35be8f6397f7ac1066f1_5fd11d4221" tabindex="-1" value=""></div>
+                    <div class="clear"><input type="submit" value="Subscribe to Newsletter!" name="subscribe" id="mc-embedded-subscribe" class="button" style="width: 100%;background-color:#ff0097;"></div>
+                </div>
+            </form>
+        </div>
+        <!--End mc_embed_signup-->
+    </ul>
+
+</div>
 
 <div class="card p-5 my-3">links card</div>
 
