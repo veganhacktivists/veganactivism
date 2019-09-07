d02e7d74b2eb6ed5917553b8625b0ed6370a3fe0
Vasco Martins <vascomartins37@gmail.com>
2019-09-07 11:36:05 +0100

    Added dynamic variables support and removed border

--- resources/views/components/org-card.blade.php
+++ resources/views/components/org-card.blade.php
@@ -1,13 +1,13 @@
 <div class="col-md-6 col-lg-6">
-  <div class="card my-3">
+  <div class="card my-3 border-0">
     <img src={{$organization->image_url}} class="card-img-top" alt={{ $organization->title }}>
     <div class="card-body">
       <h4 class="card-title"><a href="#" class="text-dark font-weight-bold">{{ $organization->title }}</a></h4>
-      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut  ullamco laboris nisi ut aliquip ex ea  anim id est laborum.</p>
+      <p class="card-text">{{ $organization->card_content }}</p>
 
       <div class="my-2">
-        <a href="#" class="btn btn-dark py-1"><i class="fas fa-info-circle mr-2"></i>Learn More</a>
-        <a href="#" class="btn btn-danger ml-3 py-1"><i class="fas fa-link mr-2"></i>Visit Website</a>
+        <a href={{ $organization->website }} class="btn btn-dark py-1"><i class="fas fa-info-circle mr-2"></i>Learn More</a>
+        <a href={{ $organization->website }} class="btn btn-danger ml-3 py-1"><i class="fas fa-link mr-2"></i>Visit Website</a>
       </div>
       <p class="card-text"><small class="text-muted">STREET ACTIVISM - </small> <small><strong>400</strong> Activists clicked on this.</small></p>
     </div>
