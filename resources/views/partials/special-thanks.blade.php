5a56f1cee4d98db5605683412cce5ef82a7418bc
Prateek Vasireddy <prateek@dave.com>
2019-09-11 12:42:53 -0700

    switch to bootstrap utility classes

--- resources/views/partials/special-thanks.blade.php
+++ resources/views/partials/special-thanks.blade.php
@@ -1,3 +1,3 @@
-<div class="card border rounded flex flex-row align-items-center" style="background-color: #f9f9f9;color: #1e205a;padding: 10px 11px 8px 13px;width: 100%;margin-left: 15px;margin-right: 15px;border-radius: 4px;">
+<div class="card border rounded flex flex-row align-items-center p-2 bg-color-gray">
   <i class="far fa-smile-beam"></i><i>&nbsp;Special thanks to the organizations partnered with us to keep their pages updated!</i>
 </div>
