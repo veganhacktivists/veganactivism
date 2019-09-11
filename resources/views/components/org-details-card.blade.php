e2eb84d5f316b27b17b40a81a633a312a581d690
Vasco Martins <vascomartins37@gmail.com>
2019-09-11 17:43:36 +0100

    Created organization details card blade

---
+++ resources/views/components/org-details-card.blade.php
@@ -0,0 +1,28 @@
+<div class="card m-3 p-0 col" >
+    <div class="embed-responsive embed-responsive-16by9">
+        <img src="https://picsum.photos/id/237/1920/1080" class="card-img-top embed-responsive-item" alt="...">
+    </div>
+    <div class="card-body">
+        <h4 class="card-title font-weight-bold">Get involed with: Organization</h4>
+        <hr>
+        <h2 class="font-weight-bold">What are they all about?</h2>
+        <p class="card-text ml-3">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+        <h2 class="font-weight-bold">How do I get involved?</h2>
+        <p class="card-text ml-3">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+        <div class="my-3 row">
+            <a href="" class="btn btn-danger ml-3 my-1 py-1"><i class="fas fa-link mr-2"></i>Visit Website</a>
+            <a href="" class="btn btn-dark ml-3 my-1 py-1"><i class="fa fa-info-circle mr-2"></i>Learn More</a>
+            <a href="" class="btn btn-dark ml-3 my-1 py-1"><i class="fab fa-facebook-square mr-2"></i>Visit Website</a>
+            <a href="" class="btn btn-dark ml-3 my-1 py-1"><i class="fas fa-envelope mr-2"></i>Contact</a>
+        </div>
+
+        <hr>
+
+        <div class="post-navigation">
+            <div class="small text-muted my-2">VEGANACTIVISM.ORG</div>
+            <a href="" class="font-weight-bold primary"><i class="fas fa-arrow-left"></i> Go back to homepage</a>
+        </div>
+    </div>
+</div>
