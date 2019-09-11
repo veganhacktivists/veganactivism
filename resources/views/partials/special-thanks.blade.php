b24cb4ec0b8ea16a95499ef9b8a3f33810f089d1
Prateek Vasireddy <prateek@dave.com>
2019-09-10 19:20:32 -0700

    basic card in place. now need the correct font

--- resources/views/partials/special-thanks.blade.php
+++ resources/views/partials/special-thanks.blade.php
@@ -1,3 +1,3 @@
-<font style="background-color: #f9f9f9;color: #1e205a;padding: 10px 11px 8px 13px;width: 100%;margin-left: 15px;margin-right: 15px;border-radius: 4px;">
-  <i class="far fa-smile-beam"></i>&nbsp;Special thanks to the organizations partnered with us to keep their pages updated!
-</font>
+<div class="card border rounded flex flex-row align-items-center" style="background-color: #f9f9f9;color: #1e205a;padding: 10px 11px 8px 13px;width: 100%;margin-left: 15px;margin-right: 15px;border-radius: 4px;">
+  <i class="far fa-smile-beam"></i><span class="">&nbsp;Special thanks to the organizations partnered with us to keep their pages updated!</span>
+</div>
