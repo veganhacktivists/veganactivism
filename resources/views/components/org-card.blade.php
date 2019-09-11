53f3cb417f6857def29bc79d51f68835f3f4ca9d
Vasco Martins <vascomartins37@gmail.com>
2019-09-11 22:30:07 +0100

    [upd] Organizations card design

--- resources/views/components/org-card.blade.php
+++ resources/views/components/org-card.blade.php
@@ -5,9 +5,9 @@
       <h4 class="card-title"><a href="#" class="text-dark font-weight-bold">{{ $organization->title }}</a></h4>
       <p class="card-text">{{ $organization->card_content }}</p>
 
-      <div class="my-2">
-        <a href={{ $organization->website }} class="btn btn-dark py-1"><i class="fas fa-info-circle mr-2"></i>Learn More</a>
-        <a href={{ $organization->website }} class="btn btn-danger ml-3 py-1"><i class="fas fa-link mr-2"></i>Visit Website</a>
+      <div class="my-2 mx-1 row">
+        <a href="pages/{{ $organization->slug }}" class="btn btn-dark py-1 "><i class="fas fa-info-circle mr-1"></i>Learn More</a>
+        <a href="{{ $organization->website }}" class="btn btn-danger ml-3 py-1"><i class="fas fa-link mr-1"></i>Visit Website</a>
       </div>
       <p class="card-text"><small class="text-muted">STREET ACTIVISM - </small> <small><strong>400</strong> Activists clicked on this.</small></p>
     </div>
