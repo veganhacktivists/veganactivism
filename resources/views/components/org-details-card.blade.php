6da6702a91dbf19f6a55ea335d4f999d7c418d7e
Vasco Martins <vascomartins37@gmail.com>
2019-09-12 19:47:36 +0100

    [upd] Added margins and better button organization

--- resources/views/components/org-details-card.blade.php
+++ resources/views/components/org-details-card.blade.php
@@ -6,15 +6,15 @@
         <h4 class="card-title font-weight-bold">Get involed with: Organization</h4>
         <hr>
         <h2 class="font-weight-bold">What are they all about?</h2>
-        <p class="card-text ml-3">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 
         <h2 class="font-weight-bold">How do I get involved?</h2>
-        <p class="card-text ml-3">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 
-        <div class="my-3 row">
-            <a href="" class="btn btn-danger ml-3 my-1 py-1"><i class="fas fa-link mr-2"></i>Visit Website</a>
-            <a href="" class="btn btn-dark ml-3 my-1 py-1"><i class="fa fa-info-circle mr-2"></i>Learn More</a>
-            <a href="" class="btn btn-dark ml-3 my-1 py-1"><i class="fab fa-facebook-square mr-2"></i>Visit Website</a>
+        <div class="row justify-content-center">
+            <a href="" target="_blank" class="btn btn-danger my-1 py-1"><i class="fas fa-link mr-2"></i>Visit Homepage</a>
+            <a href="" class="btn btn-dark ml-3 my-1 py-1"><i class="fas fa-calendar-alt mr-2"></i>View Events</a>
+            <a href="" target="_blank" class="btn btn-dark ml-3 my-1 py-1"><i class="fab fa-facebook-square mr-2"></i>Facebook</a>
             <a href="" class="btn btn-dark ml-3 my-1 py-1"><i class="fas fa-envelope mr-2"></i>Contact</a>
         </div>
 
