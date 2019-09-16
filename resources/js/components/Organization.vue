7ac1427f3e2a1ed4c9f0bc7fb5069f13ba9be815
Prateek Vasireddy <prateek@dave.com>
2019-09-16 13:14:11 -0700

    use named show route everywhere. for now use model accessors to pass things to vue. refactor livewire once i test that out

--- resources/js/components/Organization.vue
+++ resources/js/components/Organization.vue
@@ -6,12 +6,12 @@
             <div class="card my-3 border-0">
                 <img :src="org.image_card_url" class="card-img-top" :alt="org.title">
                 <div class="card-body">
-                    <h4 class="card-title"><a :href="detailsLink(org.slug)" class="text-dark font-weight-bold">{{ org.title }}</a></h4>
+                    <h4 class="card-title"><a :href="org.show_route" class="text-dark font-weight-bold">{{ org.title }}</a></h4>
                     <p class="card-text">{{ org.card_content}}</p>
 
                     <div class="my-2 mx-1 row">
-                        <a :href="detailsLink(org.slug)" class="btn btn-dark py-1 "><i class="fas fa-info-circle mr-1"></i>Learn More</a>
-                        <a :href="org.website" target="_blank" class="btn btn-danger ml-3 py-1"><i class="fas fa-link mr-1"></i>Visit Website</a>
+                        <a :href="org.show_route" class="btn btn-dark py-1 "><i class="fas fa-info-circle mr-1"></i>Learn More</a>
+                        <a :href="org.website_url" target="_blank" class="btn btn-danger ml-3 py-1"><i class="fas fa-link mr-1"></i>Visit Website</a>
                     </div>
                     <p class="card-text"><small class="text-muted">STREET ACTIVISM - </small> <small><strong>400</strong> Activists clicked on this.</small></p>
                 </div>
@@ -28,19 +28,7 @@
     export default {
         name: "organization",
 
-        props: ['data', 'showRoute'],
-
-        mounted() {
-            console.log(this.data)
-        },
-
-        methods: {
-
-            detailsLink: function(slug) {
-                return 'pages/' + slug
-            }
-
-        }
+        props: ['data']
     }
 </script>
 
