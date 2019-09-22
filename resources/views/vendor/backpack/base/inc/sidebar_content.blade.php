3e8da6a4d1810f1f1507d7c7c3883ef0d5df6524
MariaHCD <mariahcdcosta@gmail.com>
2019-09-22 22:21:26 +0400

    Remove file manager from admin sidebar

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -18,14 +18,6 @@
     </a>
 </li>
 
-
-<li>
-    <a href="{{ backpack_url('elfinder') }}">
-        <i class="fa fa-files-o"></i>
-        <span>{{ trans('backpack::crud.file_manager') }}</span>
-    </a>
-</li>
-
 <li class="treeview">
     <a href="#">
         <i class="fa fa-group"></i>
