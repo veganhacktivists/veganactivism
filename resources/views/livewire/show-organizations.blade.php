45738f87df7b0e6814b4d3a2ebd8965768aed1cd
Vasco Martins <vascomartins37@gmail.com>
2019-09-24 22:53:20 +0100

    [add] Featured badge

--- resources/views/livewire/show-organizations.blade.php
+++ resources/views/livewire/show-organizations.blade.php
@@ -10,12 +10,17 @@
     <div class="row">
         @foreach ($organizations as $organization)
             <div  class="col-md-6 col-lg-6">
-                <div class="card my-3 border-0">
+                    <div class="card my-3 border-0">   
                     <img src={{ $organization['image_card_url']}} class="card-img-top" alt={{ $organization['title'] }}>
                     <div class="card-body">
-                        <h4 class="card-title"><a href="{{ $organization['show_route'] }}"  class="text-dark font-weight-bold">{{ $organization['title'] }}</a></h4>
+                        <h4 class="card-title"><a href="{{ $organization['show_route'] }}"  class="text-dark font-weight-bold">{{ $organization['title'] }} 
+                        
+                            @if($organization['featured'] == 1)
+                                <span class="badge badge-primary">Featured</span>
+                            @endif
+                        
+                        </a></h4>
                         <p class="card-text">{{ $organization['card_content'] }}</p>
-
                         <div class="my-2 mx-1 row">
                             <a href="pages/" class="btn btn-dark py-1 "><i class="fas fa-info-circle mr-1"></i>Learn More</a>
                             <a href="" target="_blank" class="btn btn-danger ml-3 py-1"><i class="fas fa-link mr-1"></i>Visit Website</a>
