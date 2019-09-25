f13393e72b0b602edd01bf65cdb3dad1b2ec21e8
Prateek Vasireddy <prateek@dave.com>
2019-09-24 18:44:17 -0700

    address naima feedback

--- resources/views/components/links/instagram.blade.php
+++ resources/views/components/links/instagram.blade.php
@@ -1,1 +1,1 @@
-<a href="{{ $url }}?ref=veganactivism.org" target="_blank" class="btn btn-danger my-1 py-1"><i class="fas fa-link mr-2"></i>Visit Homepage</a>
+<a href="{{ $url }}?ref=veganactivism.org" target="_blank" class="btn btn-dark my-1 py-1"><i class="fas fa-instagram mr-2"></i>Instagram</a>
