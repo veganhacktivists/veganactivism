9bf136b3f9a28f12a8b4c0280ac32f5b56dd30a4
MariaHCD <mariahcdcosta@gmail.com>
2019-09-28 21:50:16 +0400

    Update Roles seeder to be more readable

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -3,6 +3,7 @@
 use App\Models\BackpackUser;
 use App\Role;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 
 class RolesTableSeeder extends Seeder
 {
@@ -12,9 +13,30 @@ class RolesTableSeeder extends Seeder
     public function run()
     {
         $role = Role::create(['name' => BackpackUser::ROLE_SUPER_ADMIN]);
-        $role->permissions()->attach([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+        $superAdminPermissionIds = Permission::whereIn('name', [
+            BackpackUser::PERMISSION_USERS_VIEW,
+            BackpackUser::PERMISSION_USERS_CREATE,
+            BackpackUser::PERMISSION_USERS_EDIT,
+            BackpackUser::PERMISSION_USERS_DELETE,
+            BackpackUser::PERMISSION_ORGANIZATIONS_VIEW,
+            BackpackUser::PERMISSION_ORGANIZATIONS_CREATE,
+            BackpackUser::PERMISSION_ORGANIZATIONS_EDIT,
+            BackpackUser::PERMISSION_ORGANIZATIONS_DELETE,
+            BackpackUser::PERMISSION_LINKS_VIEW,
+            BackpackUser::PERMISSION_LINKS_CREATE,
+            BackpackUser::PERMISSION_LINKS_EDIT,
+            BackpackUser::PERMISSION_LINKS_DELETE,
+        ])->pluck('id')->toArray();
+        $role->permissions()->attach($superAdminPermissionIds);
 
         $role = Role::create(['name' => BackpackUser::ROLE_ADMIN]);
-        $role->permissions()->attach([6, 7, 9, 10, 11]);
+        $adminPermissionIds = Permission::whereIn('name', [
+            BackpackUser::PERMISSION_ORGANIZATIONS_VIEW,
+            BackpackUser::PERMISSION_ORGANIZATIONS_EDIT,
+            BackpackUser::PERMISSION_LINKS_VIEW,
+            BackpackUser::PERMISSION_LINKS_CREATE,
+            BackpackUser::PERMISSION_LINKS_EDIT
+        ])->pluck('id')->toArray();
+        $role->permissions()->attach($adminPermissionIds);
     }
 }
