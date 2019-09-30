1f7ada6c712763289c755d776d8034bb0c68e475
Naima H <naimahamouma@gmail.com>
2019-10-01 01:15:23 +0200

    Set cards links correctly

--- resources/views/livewire/show-organizations.blade.php
+++ resources/views/livewire/show-organizations.blade.php
@@ -13,7 +13,7 @@
                     <div class="card my-3 border-0">   
                     <img src={{ $organization['image_card_url']}} class="card-img-top" alt={{ $organization['title'] }}>
                     <div class="card-body">
-                        <h4 class="card-title"><a href="{{ $organization['show_route'] }}"  class="text-dark font-weight-bold">{{ $organization['title'] }} 
+                        <h4 class="card-title"><a href={{ $organization['show_route'] }}  class="text-dark font-weight-bold">{{ $organization['title'] }} 
                         
                             @if($organization['featured'] == 1)
                                 <span class="badge badge-primary">Featured</span>
@@ -22,8 +22,8 @@
                         </a></h4>
                         <p class="card-text">{{ $organization['card_content'] }}</p>
                         <div class="my-2 mx-1 row">
-                            <a href="pages/" class="btn btn-dark py-1 "><i class="fas fa-info-circle mr-1"></i>Learn More</a>
-                            <a href="" target="_blank" class="btn btn-danger ml-3 py-1"><i class="fas fa-link mr-1"></i>Visit Website</a>
+                            <a href={{ $organization['show_route'] }} class="btn btn-dark py-1 "><i class="fas fa-info-circle mr-1"></i>Learn More</a>
+                            <a href={{ $organization['website_url'] }} target="_blank" class="btn btn-danger ml-3 py-1"><i class="fas fa-link mr-1"></i>Visit Website</a>
                         </div>
                     <p class="card-text"><small class="text-muted">STREET ACTIVISM - </small> <small><strong>{{ $organization['website_clicks']}}</strong> Activists clicked on this.</small></p>
                     </div>
