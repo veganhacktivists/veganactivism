f206777d9be6cddc354177b399dce1c4ab91e027
David van Beveren <david@veganhacktivists.org>
2019-10-15 10:54:23 +0200

    Line height + remove activism type

--- resources/views/components/org-card.blade.php
+++ resources/views/components/org-card.blade.php
@@ -2,14 +2,14 @@
   <div class="card my-3 border-0">
     <img src={{$organization->image_url}} class="card-img-top" alt={{ $organization->title }}>
     <div class="card-body">
-      <h4 class="card-title"><a href="{{ route('organizations.show', $organization->slug) }}" class="text-dark font-weight-bold" style="line-height: 1.45;">{{ $organization->title }}</a></h4>
+      <h4 class="card-title"><a href="{{ route('organizations.show', $organization->slug) }}" class="text-dark font-weight-bold" style="line-height: 1.5 !important;">{{ $organization->title }}</a></h4>
       <p class="card-text">{{ $organization->card_content }}</p>
 
       <div class="my-2 mx-1 row">
         <a href="{{ route('organizations.show', $organization->slug) }}" class="btn btn-dark py-1 "><i class="fas fa-info-circle mr-1"></i>Learn More</a>
         <a href="{{ $organization->website }}" target="_blank" class="btn btn-danger ml-3 py-1"><i class="fas fa-link mr-1"></i>Visit Website</a>
       </div>
-      <p class="card-text"><small class="text-muted">STREET ACTIVISM - </small> <small><strong>400</strong> Activists clicked on this.</small></p>
+      <p class="card-text"><small class="text-muted"><small><strong>400</strong> Activists clicked on this.</small></p>
     </div>
   </div>
 </div>
