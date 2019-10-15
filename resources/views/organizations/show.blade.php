121b1df3458e89f396ea25ab815b4c8bc3e95e93
David van Beveren <david@veganhacktivists.org>
2019-10-15 13:52:59 +0200

    Implimented Nav Info Bar on org pages

--- resources/views/organizations/show.blade.php
+++ resources/views/organizations/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="col-lg-8">
-        @include('partials.welcome-card')
+        @include('partials.nav-info-bar')
 
         <div class="row">
             @include('components.org-details-card')
---
+++ resources/views/partials/nav-info-bar.blade.php
@@ -0,0 +1,8 @@
+<div class="card my-3">
+  <div class="card-body pl-4 pr-4">
+    <b><i class="fas fa-leaf"></i></b><b>&nbsp;&nbsp;
+
+      Please <a href="https://www.patreon.com/veganhacktivists" target="_blank">consider a donation</a>
+      if you find our site useful!</i>&nbsp;&nbsp;<i class="fas fa-hand-holding-heart"></i>
+  </div>
+</div>
