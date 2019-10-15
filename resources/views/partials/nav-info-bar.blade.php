548e1baad095c105ac5295054adccc840aec190b
David van Beveren <david@veganhacktivists.org>
2019-10-15 14:05:55 +0200

    Cleaned up margins around the info bar

--- resources/views/partials/nav-info-bar.blade.php
+++ resources/views/partials/nav-info-bar.blade.php
@@ -1,4 +1,4 @@
-<div class="card my-3">
+<div class="card my-3" style="margin-bottom: 0px;">
   <div class="card-body pl-4 pr-4">
   <a href="/" class="font-weight-bold primary"><i class="fas fa-arrow-left"></i>&nbsp;&nbsp;Go back to homepage</a>
   <b style="float:right;padding-left: 10px;"><i class="fas fa-leaf"></i></b><a style="float:right;" href="https://www.patreon.com/veganhacktivists" target="_blank">Consider a donation if you can!</a>
