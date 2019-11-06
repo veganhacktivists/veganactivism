1c34dd51c47636067872ec64b8f9703fccf3ea2a
Prateek Vasireddy <prateek@dave.com>
2019-11-05 20:13:45 -0800

    redis poc to dd the data

--- app/Events/OrgLinkClicked.php
+++ app/Events/OrgLinkClicked.php
@@ -5,14 +5,11 @@ namespace App\Events;
 use App\Organization;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Broadcasting\InteractsWithSockets;
 
 class OrgLinkClicked
 {
     use Dispatchable;
-    use InteractsWithSockets;
     use SerializesModels;
 
     private $organization;
@@ -27,15 +24,5 @@ class OrgLinkClicked
         $this->organization = $organization;
 
         $this->user = $user;
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('org-link-clicks');
     }
 }
