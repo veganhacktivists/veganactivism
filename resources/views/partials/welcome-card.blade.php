523a3e2cf508620ff9f31713a42bfbda36a7a635
David van Beveren <admin@veganhacktivists.org>
2022-12-05 23:50:43 +0100

    Clean up introduction header

--- resources/views/partials/welcome-card.blade.php
+++ resources/views/partials/welcome-card.blade.php
@@ -2,14 +2,6 @@
   <div class="card-body pl-4 pr-4">
     <b><i class="fas fa-leaf"></i></b><b>&nbsp;&nbsp;
       Welcome to <a href="{{route('home')}}">VeganActivism.org</a></b>,
-    the most comprehensive verified list of Vegan Activism organizations online for Vegans looking to jump into activism.
-
-    Whether you have a lot of time or little, whether you're new or experienced, there's an organization here that's looking for your help to spread our compassionate vegan message.
-    <br> <br>
-    <i> This site is an ad-less project by the
-      <a href="https://VeganHacktivists.org" target="_blank">VeganHacktivists.org</a>,
-      which is funded by its Patreon supporters.
-      Please <a href="https://www.patreon.com/veganhacktivists" target="_blank">consider a donation</a>
-      if you find our site useful!</i>&nbsp;&nbsp;<i class="fas fa-hand-holding-heart"></i>
+      a verified list of vegan organizations from around the world! Whether you have a lot of time to commit to the animals or just a little, there's an organization on our list that aligns with your goals and interests and is looking for your help to spread the compassionate vegan message.
   </div>
 </div>
