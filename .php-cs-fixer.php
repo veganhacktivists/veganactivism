59069f4fac87d217eee845e4855f01ff40b71241
Jérémy Touati <sielver@gmail.com>
2024-05-13 15:22:16 +0200

    Fix wrong PHP CS Fixer format

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -8,7 +8,7 @@ $finder = PhpCsFixer\Finder::create()
     ->in('lib')
 ;
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR1' => true,
         '@PSR2' => true,
